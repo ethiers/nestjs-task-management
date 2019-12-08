289fd19ac7c53c7111095ee16ba60c4b8828a2d1
Sebastien Ethier <sebastien.ethier@seconceptweb.ca>
2019-12-08 15:57:49 -0500

    Section 8: (Bonus) Pro-Production: Configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,7 +4,7 @@ import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {JwtPayload} from './jwt-payload.interface';
 import {InjectRepository} from '@nestjs/typeorm';
 import {UserRepository} from './user.repository';
-import {User} from './user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'), // retrieve the secret to validate
         } as StrategyOptions);
     }
 
